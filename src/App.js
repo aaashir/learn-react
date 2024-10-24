bec22c6e9feb583c08ad3db4e4fbaa6642503a6e
Aashir Shaikh <hi@aashir.me>
2024-10-24 21:43:15 +0500

    pilot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [countdownValue, setCountdownValue] = useState(0)
+
+  const handleSubtract = () => {
+    if(countdownValue >=1) {
+      setCountdownValue(countdownValue-1)
+    }
+  }
+
+  const handleAdd = () => {
+    setCountdownValue(countdownValue+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="timer_container">
+        <h1>Countdown App</h1>
+
+        <div className="countdown_container">
+          <p className="countdown_label">{countdownValue}</p>
+          <div className="buttons">
+            <button onClick={handleSubtract}>-</button>
+            <button onClick={handleAdd}>+</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
